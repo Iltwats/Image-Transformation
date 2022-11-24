e11b05d923df553f9daa66f887ab6cf609fa48d7
Atul Sharma <atul.19bcon231@jecrcu.edu.in>
2022-11-24 09:18:08 +0000

    added comments

--- ImageTransform.h
+++ ImageTransform.h
@@ -3,6 +3,8 @@
 #include "uiuc/PNG.h"
 using namespace uiuc;
 
+// Functions doing image transformation
+
 PNG grayscale(PNG image);  
 PNG createSpotlight(PNG image, int centerX, int centerY);
 PNG illinify(PNG image);
